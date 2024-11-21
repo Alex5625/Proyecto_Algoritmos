3956fe5ac5705435bdb0b5eb3986267c30fe4288
preciousdep <aosthetic@gmail.com>
2024-11-20 22:12:17 -0300

    draft de alineamiento

--- main.cpp
+++ main.cpp
@@ -21,8 +21,6 @@ int alineamiento(int** puntuacion, int** matriz, int gap_score){
     */
 }
 
-
-
 void imprimir_matriz(int** matriz, int fila, int columna){
 	for (int i = 0; i < fila; ++i) {
 		for (int j = 0; j < columna; ++j) {
@@ -30,7 +28,53 @@ void imprimir_matriz(int** matriz, int fila, int columna){
         }
         std::cout << std::endl;
     }
+}
 
+int valorPuntuacion(char a, char b){
+    // usar con la matriz de puntuacion, ns como usarla
+    if (a==b){
+        return 2;
+    } else {
+        return -1;
+    };
+}
+
+// maximo para recorrer las secuencias
+int maximo(int a, int b, int c){ // uhh idk
+    if (a > b && a > c){
+        return a;
+    } else if (b>a && b > c){
+        return b;
+    } else if (c>a && c>b){
+        return c;
+    } else if (c > a && b == c){
+        return c; // valor sin gap
+    } else if (c > b && c == a){
+        return c;
+    }
+}
+
+int** draftMatrizAlineada(const char* cad1, const char* cad2, int** matriz, int gap_score){
+    int columna = strlen(cad1) + 1;
+    int fila = strlen(cad2) + 1;
+
+    for (int i = 0; i<=fila; i++){
+        for (int j = 0;j<=columna;j++){
+            if (i==0 && j==0){
+                matriz[i][j] = 0;
+            } else if (i>0 && j == 0){
+                matriz[i][j] = matriz[i-1][j] + gap_score;
+            } else if (i==0 && j>0){
+                matriz[i][j] = matriz[i][j-1] + gap_score;
+            } else if (i>0 && j>0) {
+                matriz[i][j] = maximo(
+                    matriz[i-1][j]+gap_score,
+                    matriz[i][j-1]+gap_score,
+                    matriz[i][j]+valorPuntuacion(cad1[i],cad2[j]));
+            }
+        } 
+    }    
+    imprimir_matriz(matriz,fila,columna);
 }
 
 
@@ -58,7 +102,7 @@ int** generar_matriz_secuencias(const char* cad1, const char* cad2){
     return matriz;
 }
 
-
+// genera matriz 4x4 con los puntajes, donde 2 para match y -1 mismatch
 int** generar_Funcion(string nombreArchivo){
 	
 	ifstream file(nombreArchivo); // archivo de puntajes
@@ -96,6 +140,9 @@ int** generar_Funcion(string nombreArchivo){
 
 //  0              1            2             3        4
 //./programa -C1 cad1.tex -C2 cad2.tex -U matriz.tex -V val
+
+//./programa cad.tex cad2.tex matriz.tex -1
+
 int main(int argc, char **argv) {
     if (argc < 3) {
         cout << "no añadiste los archivos.\n";
@@ -120,6 +167,8 @@ int main(int argc, char **argv) {
     
 
     int** matriz_secuencia = generar_matriz_secuencias(cadena1, cadena2);
-
-    alineamiento(similitud, matriz_secuencia, stoi(argv[4]));
+    
+    draftMatrizAlineada(cadena1,cadena2,matriz_secuencia,stoi(argv[4]));
+    // estoy probando aparte para organizarlo despues
+    //alineamiento(similitud, matriz_secuencia, stoi(argv[4]));
 }
